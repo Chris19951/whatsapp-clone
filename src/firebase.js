bf044e79b7c2f3d57463e7bc72e8af744c9add1b
Premade24 <94998435+Chris19951@users.noreply.github.com>
2022-07-08 16:28:19 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,8 @@ import {getDatabase} from "firebase/database";
 import 'firebase/compat/auth'
 import {getStorage,ref} from "firebase/storage"
 
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyAadISmhV2sCfScxBohh8xT1bzCBdCTwVQ",
-  authDomain: "premade24-f8d8e.firebaseapp.com",
-  databaseURL: "https://premade24-f8d8e-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "premade24-f8d8e",
-  storageBucket: "premade24-f8d8e.appspot.com",
-  messagingSenderId: "573768177973",
-  appId: "1:573768177973:web:2ac8994cb1e47a1256147c",
-  measurementId: "G-HTY543EB0D",
-})
+//put in your config
+
 
 export const auth = app.auth()
 export const db = getDatabase(app)
